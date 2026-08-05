ee418453e223e29cbf66db94873cf11ebc83f4e2
Sean07H <152515897+Sean07H@users.noreply.github.com>
2026-08-04 22:48:54 -0400

    Add date and greeting display functionality

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
+let now = new Date();
 
+
+document.getElementById("date").innerHTML =
+now.toDateString();
+
+
+
+let hour = now.getHours();
+
+
+let greeting;
+
+
+if(hour < 12){
+
+greeting="Good Morning";
+
+}
+
+else if(hour < 18){
+
+greeting="Good Afternoon";
+
+}
+
+else{
+
+greeting="Good Evening";
+
+}
+
+
+
+document.getElementById("greeting").innerHTML=greeting;
